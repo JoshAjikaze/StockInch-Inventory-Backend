7fe7375d6c805d80e45a2babd5385455c00f8b87
Uteehacks <uteejacob@gmail.com>
2024-09-09 20:53:24 +0400

    image fix

--- inventory_app/settings.py
+++ inventory_app/settings.py
@@ -230,7 +230,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
